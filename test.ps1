3669dfa8bf64f7fc208a6cbb54457b665d6ac3eb
Adam <almcchesney@gmail.com>
2018-09-16 19:37:14 -0500

    set to confirm install-module errors now trigger non zero exit code

--- test.ps1
+++ test.ps1
@@ -1,6 +1,13 @@
-# Install Dependencies
-Install-Module PSScriptAnalyzer
-Get-ChildItem -Recurse ./src |
-    ForEach-Object{
-        Invoke-ScriptAnalyzer $_.FullName
-    }
+# Set EAP to stop to catch errors and exit with an error code
+$ErrorActionPreference= Stop
+try{
+    # Install Dependencies
+    Install-Module PSScriptAnalyzer -Confirm
+    Get-ChildItem -Recurse ./src |
+        ForEach-Object{
+            Invoke-ScriptAnalyzer $_.FullName
+        }
+} catch {
+    # TODO: Setup and use error codes
+    exit 1
+}
